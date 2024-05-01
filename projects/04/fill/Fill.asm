c340474841b568271389e355011cdc7e4bccbe6b
wcscherer <wscherer25@gmail.com>
2024-04-30 22:06:17 -0400

    Last big push to main

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// Pointer to Memory location for keyboard map
+@8192 // (512*32)/16
+D = A
+@board
+M = D
+
+(LOOP)
+// Set pixel array index counter to 0
+    @i
+    M = 0
+(CHECK)
+// If key != pressed
+// GOTO set Pixel WHITE
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ   // goto WHITE if KBD value is 0
+
+(BLACK)
+// SET pixel to black
+    // if counter = end of pixel length skip increment - GOTO LOOP
+    // GOTO END
+    @i
+    D=M
+
+    @SCREEN
+    // Find current address
+    A=A+D   
+    // Fill with black
+    M=-1    
+    @END
+    0;JMP   // goto END
+
+(WHITE)
+// SET pixel to white
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M = 0
+
+(END)
+    // increment index counter
+    @i
+    MD = M + 1
+    // decrement board counter
+    @board
+    D = D - M
+    // goto LOOP if the character set in board - i == 0
+    @LOOP
+    D;JEQ   
+    // goto INNER
+    @INNER
+    0;JMP   
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -5,8 +5,35 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Create intermediate counter variable j and set R2 to 0
+    @j
+    M=0
+    @R2
+    M=0
+// Loop through adding R0 to R2 R1 times
+(LOOP)
+// If j <= R1 goto END
+    @j
+    D=M
+    @R1
+    D=D-M // D = D-R1
+    @END
+    D;JGE // if i - R1 == 0 end
+// Add R0 to R2
+    @R0
+    D=M
+    @R2
+    M=D+M
+// Increment j = j + 1
+    @j
+    M = M + 1
+// If j < R1 goto LOOP
+    @LOOP
+    0;JMP
+// Leave Calculation Loop
+(END)
+    @END
+    0;JMP
